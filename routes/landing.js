548271e776320129777b97d72b80a40d2e2bdbcc
dwoong93 <darylwoong@gmail.com>
2021-10-23 00:32:29 +0000

    added bootstrap and other routes

--- routes/landing.js
+++ routes/landing.js
@@ -5,7 +5,14 @@ const router = express.Router();
 //Express Router
 router.get('/', (req,res)=>{
     res.render('landing/index')
-})
+});
 
+router.get('/about', (req,res)=>{
+    res.render('landing/about')
+});
+
+router.get('/catalog', (req,res)=>{
+    res.render('landing/catalog')
+});
 //Export Router
 module.exports = router;
