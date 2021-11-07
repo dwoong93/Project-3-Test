13f4dce509d975b38e6d6fdce956cbaba32253ea
dwoong93 <darylwoong@gmail.com>
2021-11-07 23:45:54 +0000

    update or CRUD

--- routes/products.js
+++ routes/products.js
@@ -34,7 +34,6 @@ router.post('/keyboardcases/createproduct', async(req,res)=>{
             const product = new Keyboardcase();
             product.set('name', form.data.name);
             product.set('material', form.data.material);
-            product.set('name', form.data.name);
             product.set('size', form.data.size);
             product.set('keyboardKit', form.data.keyboardKit);
             product.set('quantity', form.data.quantity);
@@ -50,6 +49,58 @@ router.post('/keyboardcases/createproduct', async(req,res)=>{
         }
     })
 })
+router.get('/:product_id/update', async (req, res) => {
+    const productId = req.params.product_id;
+    const keebCases = await Keyboardcase.where({
+        'id': productId}).fetch({
+            require: true
+        });
+        console.log(productId);
+        console.log(keebCases)
+
+
+        // const productForm = createProductForm();
+
+        // productForm.fields.name.value = Keyboardcase.get('name');
+        // productForm.fields.material.value = Keyboardcase.get('material');
+        // productForm.fields.size.value = Keyboardcase.get('size');
+        // productForm.fields.keyboardKit.value = Keyboardcase.get('keyboardKit');
+        // productForm.fields.quantity.value = Keyboardcase.get('quantity');
+        // productForm.fields.cost.value = Keyboardcase.get('cost');
+        // productForm.fields.description.value = Keyboardcase.get('description');
+
+        // res.render('products/update', {
+        //     'form': productForm.toHTML(bootstrapField),
+        //     'product': product.toJSON()
+        // })
+        res.render('products/update')
+    })
+//process update
+router.post('/:product_id/update', async (req, res) => {
+    const keebCases = await Keyboardcase.where({
+        'id': productId}).fetch({
+            require: true
+        });
+    const productForm = createProductForm();
+    productForm.handle(req,{
+        'success': async (form)=>{
+            product.set(form.data);
+            product.save();
+            res.redirect('/products/keyboardcases');
+        },
+        'error':async (form) =>{
+            res.render('products/update',{
+                'form': form.toHTML(bootstrapField),
+                'keyboardcases': keebCases.toJSON()
+            })
+        }
+    })
+    
+        
+        
+
+})
+
 
 
 
