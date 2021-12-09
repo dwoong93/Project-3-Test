937164d160e771bd5669b63ea8230d652b966263
dwoong93 <darylwoong@gmail.com>
2021-12-09 13:05:54 +0000

    updated hbs files and landing page

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //Display keyboard products for Customer
 router.get('', function(req,res){
-    res.redirect('/products/catalog')
+    res.render('layouts/landingpage')
 })
 
 module.exports = router;
